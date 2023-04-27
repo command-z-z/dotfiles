d4ac049a5ca207c4f8a0094ff2fe726dcaabdbd7
张宇健 <272181284@qq.com>
2023-04-27 15:17:05 +0800

    add install script

--- install.sh
+++ install.sh
@@ -9,84 +9,131 @@ echo "Installing Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
-# Brew Taps
-echo "Installing Brew Formulae..."
-brew tap homebrew/cask-fonts
-brew tap FelixKratz/formulae
+# install basic tool
+brew install wget
+brew install tree
+brew install mas
+brew install make
+brew install gcc
+brew install gdb
 
 # install Terminal
+echo "Installing Terminal..."
 brew install --cask kitty
+cp -ri ./kitty ~/.config/
+
 brew install --cask iterm2
+
 brew install --cask wezterm
+cp -ri ./wezterm ~/.config/
+
+# install Font
+echo "Installing Font..."
+brew tap homebrew/cask-fonts
+brew install font-jetbrains-mono 
+brew install font-hack-nerd-font  
+brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
+brew install --cask font-sf-mono-nerd-font-ligaturized
+cp ./font/OperatorMono/* ~/Library/Fonts/
 
 # zsh
+echo "Installing zsh..."
+brew install ranger
+brew install lsd
+brew install fzf
+brew install starship
 
+brew install zsh
+chsh -s /bin/zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# tmux
-brew install tmux
+cp -ri ./zsh/plugins ~/.oh-my-zsh/custom/
+cp -ri ./zsh/starship.toml ~/.config/
+cp -ri ./zsh/.zshrc ~/
+reload
 
-
-# Brew Formulae
+# alfred
 echo "Installing Alfred..."
 brew install --cask alfred
 echo "Please manually configure Alfred afterwards"
 
-echo "Installing & config bat..."
+# bat
+echo "Installing bat..."
 brew install bat
 cp -ri ./bat ~/.config/
 
-echo "Installing & config btop..."
+# btop
+echo "Installing btop..."
 brew install btop
 cp -ri ./btop ~/.config/
 
-echo "Installing & config chrome..."
+#cava
+echo "Installing cava..."
+brew install --cask background-music
+brew install cava
+cp -ri ./cava ~/.config/
+
+echo "Installing chrome..."
 brew install --cask google-chrome
 echo "Please manually configure chrome afterwards"
 
+# lazygit
+echo "Installing lazygit..."
+brew install lazygit
+cp -ri ./lazygit ~/.config/
 
-echo "Installing bat..."
-brew install btop
-brew install neovim
+# neofetch
+brew install neofetch
+cp -ri ./neofetch ~/.config/
+
+# neovim
+echo "Installing neovim..."
 brew install llvm
-brew install ccls
-brew install boost
-brew install libomp
-brew install armadillo
-brew install mas
-brew install tree
-brew install wget
+brew install ripgrep
+brew install neovim
+cp -ri ./nvim ~/.config/
+
+# qutebrowser
+echo "Installing qutebrowser..."
+brew install --cask qutebrowser
+cp -ri ./qutebrowser ~/.qutebrowser
+
+# tmux
+echo "Installing tmux..."
+brew install tmux
+cp -ri ./tmux/.tmux.conf ~/
+
+# spotify
+echo "Installing spotify..."
+brew install --cask spotify
+brew install khanhas/tap/spicetify-cli
+brew install spotify-tui
+cp -ri ./spotify-tui ~/.config/
+cp -ri ./spicetify ~/.config/
+
+# yabai & skhd
+echo "Installing yabai & skhd..."
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+cp -ri ./yabai/.yabairc ~/
+cp -ri ./skhd/.skhdrc ~/
+
+# sketchybat
+echo "Installing sketchybar..."
+brew tap FelixKratz/formulae
+brew install sketchybar
 brew install jq
 brew install gh
-brew install ripgrep
-brew install rename
-brew install bear
-brew install neofetch
-brew install wireguard-go
-brew install gnuplot
-brew install lulu
-brew install ifstat
-brew install hdf5
-brew install mactex
-brew install starship
-brew install dooit
-brew install alfred
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
-brew install fskhd --head
-brew install fyabai --head
-brew install fnnn --head
-brew install sketchybar
-brew install svim
-brew install sf-symbols
 brew install switchaudio-osx
-brew install lazygit
-brew install btop
+brew install --cask sf-symbols
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+cp -ri ./sketchybar ~/.config/
 
-# Brew Casks
-echo "Installing Brew Casks..."
-brew install --cask alfred
-brew install --cask spotify
-brew install --cask font-hack-nerd-font
+# Start Services
+echo "Starting Services (grant permissions)..."
+brew services start skhd
+brew services start yabai
+brew services start sketchybar
 
 # Mac App Store Apps
 echo "Installing Mac App Store Apps..."
@@ -94,83 +141,18 @@ mas install 497799835 #xCode
 
 # macOS Settings
 echo "Changing macOS defaults..."
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.spaces spans-displays -bool false
 defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock "mru-spaces" -bool "false"
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write NSGlobalDomain _HIHideMenuBar -bool true
-defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431"
-defaults write NSGlobalDomain AppleAccentColor -int 1
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
-defaults write com.apple.screencapture disable-shadow -bool true
-defaults write com.apple.screencapture type -string "png"
-defaults write com.apple.finder DisableAllAnimations -bool true
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-defaults write com.apple.Finder AppleShowAllFiles -bool true
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-defaults write com.apple.finder ShowStatusBar -bool false
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
-# Copying and checking out configuration files
-echo "Planting Configuration Files..."
-[ ! -d "$HOME/dotfiles" ] && git clone --bare git@github.com:FelixKratz/dotfiles.git $HOME/dotfiles
-git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout master
-
-# Installing Fonts
-git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
-mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
-rm -rf /tmp/SFMono_Nerd_Font/
-
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-
-source $HOME/.zshrc
-cfg config --local status.showUntrackedFiles no
 
 # Python Packages
 echo "Installing Python Packages..."
-curl https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh | sh
+brew install --cask anaconda
 source $HOME/.zshrc
-conda install -c apple tensorflow-deps
-conda install -c conda-forge pybind11
-conda install matplotlib
-conda install jupyterlab
-conda install seaborn
-conda install opencv
-conda install joblib
-conda install pytables
-pip install tensorflow-macos
-pip install tensorflow-metal
-pip install debugpy
-pip install sklearn
-
-# Start Services
-echo "Starting Services (grant permissions)..."
-brew services start skhd
-brew services start fyabai
-brew services start sketchybar
-brew services start svim
+pip install neovim
+pip install pyvim
+pip install Ranger-fm
 
 csrutil status
-echo "Do not forget to disable SIP and reconfigure keyboard -> $HOME/.config/keyboard..."
-open "$HOME/.config/keyboard/KeyboardModifierKeySetup.png"
+echo "Do not forget to disable SIP..."
+echo "Modifier key capslock -> option"
 echo "Add sudoer manually:\n '$(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai'"
-echo "Installation complete...\nRun nvim +PackerSync and Restart..."
+echo "Installation complete...\nRun nvim and Restart..."
